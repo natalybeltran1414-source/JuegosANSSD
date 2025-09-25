19be7b686d74c9dd96efdf97aa39698d7321aea4
CRIS-web05 <andresawchicaiza@gmail.com>
2025-09-24 20:38:54 -0500

    creacion nueva rama

--- juegosanssd/urls.py
+++ juegosanssd/urls.py
@@ -4,5 +4,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('juego.urls')), 
-    # empieza con s y termina en y jajajajaj ok no 
 ]
