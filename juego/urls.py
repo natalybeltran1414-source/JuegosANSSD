f8951a20ce64975cb188c6901b31cda0d9802269
xXGUuZXx-web70 <deymiangustavo70@gmail.com>
2025-09-24 20:45:03 -0500

    ME CAES MAL

--- juego/urls.py
+++ juego/urls.py
@@ -1,7 +1,7 @@
 # juego/urls.py
 from django.urls import path
 from . import views
-
+# YA MANDEN A LA CASA 
 urlpatterns = [
     path('', views.inicio_juego, name='inicio'),
     path('pregunta/', views.pregunta_aleatoria, name='pregunta'),
